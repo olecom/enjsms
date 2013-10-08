7d5f3fc5e6869d1b72147281863505445ff14d80
Oleg Verych <olecom@gmail.com>
2013-10-08 17:45:47 +0200

    app_front.js: config file loading

---
+++ config/cfg_default.js
@@ -0,0 +1,19 @@
+config = {
+// this is not JSON just JavaScript (idea is taken from github.com/tldrio/mongo-edit)
+    mongodb: {
+         host: 'localhost'
+        ,port: 27017
+        ,journal: true
+        ,dbname: 'test'
+        ,dbpath: 'data/db'
+        ,dbprefix: ''
+    }
+    ,gsm_modules: [
+        {
+            //name
+            //addr:
+        }
+    ]
+    ,app: {
+    }
+}
--- web_assets/app_front.js
+++ web_assets/app_front.js
@@ -6,8 +6,9 @@ var gui = require('nw.gui')
 var app = {
     tray: { obj: null ,stat: 'show' },
     w: null,
-    configExtApp: null
+    config: { ExtApp: null }
 }
+var tools = {}
 
 l10n = {
     tray: {
@@ -18,14 +19,56 @@ l10n = {
     ,extjsNotFound:
         'Не найден или не загрузился "ExtJS" (визуальная библиотека).\n' +
         'Нужно проверить конфигурацию программы "enjsms".'
+    ,configLoadError:
+        'Ошибка при чтении файла конфигурации!'
 }
 
-;(function node_webkit_uglify_js_closure(con ,app ,gui){
+tools_common_uglify_js_closure(console ,app)
+
+;(function node_webkit_uglify_js_closure(con ,app ,gui ,tools){
     app.w = gui.Window.get()
     app.w.showDevTools()
 
     setup_tray(app.tray ,app.w)
-    load_extjs(app.w.window.document ,app.w.window)
+
+    load_config(app) && tools.load_extjs(app.w.window.document ,app.w.window)
+    return
+
+function load_config(app){
+    var cfg
+    var fs = require('fs')
+
+    if(process._nw_app && (cfg = process._nw_app.argv[0])){// cmd line
+        cfg = 'config/' + cfg
+    } else {// HOME config
+        if(process.env.HOME){
+            cfg = process.env.HOME
+        } else if(process.env.HOMEDRIVE && process.env.HOMEPATH){
+            cfg = process.env.HOMEDRIVE +  process.env.HOMEPATH
+        }
+        cfg = cfg + '/.enjsms.js'
+        try {
+            fs.statSync(cfg)
+        } catch (ex){
+            cfg = null
+        }
+    }
+    if(!cfg)// default
+        cfg = 'config/cfg_default.js'
+
+    try {
+        app.config = (
+            new Function('return (function(){ var config ; return ' +
+                          fs.readFileSync(cfg ,'utf8') + '})()')
+        )()
+    } catch(ex){
+        con.error('ERROR load_config:' + ex + '\n' + ex+ '\n' + cfg)
+        app.w.window.alert(l10n.configLoadError + '\n' + ex + '\n' + cfg)
+        return false
+    }
+    con.log('reading config: ' + cfg + ' done')
+    return true
+}
 
 function setup_tray(t ,w){
     t.obj = new gui.Tray({ title: l10n.tray.title ,icon: 'web_assets/images/favicon.png' })
@@ -44,9 +87,16 @@ function setup_tray(t ,w){
     })
     con.log('setup_tray: done')
 }
-})(console ,app ,gui)
+})(console ,app ,gui ,tools)
 
 //common
+
+//;(
+function tools_common_uglify_js_closure(con ,app){
+
+    tools.load_extjs = load_extjs
+    return
+
 function load_extjs(doc ,w){
 var extjs, path
 
@@ -65,16 +115,17 @@ var extjs, path
     w.addEventListener('load' ,(extjs = function(){
         w.removeEventListener('load' ,extjs)
         if('undefined' != typeof Ext){
-            console.log('ExtJS version: ' + Ext.getVersion('extjs'))
+            con.log('ExtJS version: ' + Ext.getVersion('extjs'))
             //Ext.application(app.configExtApp)
         } else {
-            console.err(l10n.extjsNotFound)
+            con.err(l10n.extjsNotFound)
             w.alert(l10n.extjsNotFound)
         }
     }))
-    console.log('load_extjs: done, waiting for ExtJS')
+    con.log('load_extjs: done, waiting for ExtJS')
 }
-
+}
+//)(console ,app)
 
 // for express part:
 /*
