83920f6a730ec5df67299dc36c6ad1ed29cac885
Oleg Verych <olecom@gmail.com>
2014-03-04 10:45:36 +0200

    cfg_default: coding style changes

--- config/cfg_default.js
+++ config/cfg_default.js
@@ -2,7 +2,7 @@ config = {
 /* NOTE: this is not JSON just JavaScript
  *       idea is taken from github.com/louischatriot/mongo-edit
  */
-    gsm_modules: [
+    gsm_modules:[
         {
             //name
             //addr:
@@ -13,39 +13,34 @@ config = {
 
     dev: 'enjsms',// developring this component
     log: 'log/',
-    app: {
+    app:{
         db: { mongodb:1 ,nedb:0 }, // github.com/louischatriot/nedb
-        modules: {// cfg things from 'app_modules'
-            procman: {
+        modules:{// cfg things from 'app_modules'
+            procman:{
                 // default: cfg_procman.js in own directory
                 config: 'config/cfg_procman_mongo_node_sms.js'
-                ,autoSpawn: true
+               ,autoSpawn: true
             }
-            ,usersman: {
+           ,usersman:{
                 store: 'dummy' //dummy (internal ro default roles), json file, db
             }
         }
     },
-    extjs: {
+    extjs:{
         name: 'App',             // default
         appFolder: '.',          // default
         launch: null             // default
-        ,pathFile: 'extjs.txt'
-        ,path: 'ext-4.2.1.883/'  // search extjs.txt or this above './'; 'extjs/' is for HTML
-        ,fading: true            // visual effects for content appearance
-        /* custom ExtJS applicaiton config...
-         * models: ['', ''],
-         * stores: ['', '', ''],
-         * controllers: ['', '']
-         */
+       ,pathFile: 'extjs.txt'
+       ,path: 'ext-4.2.1.883/'   // search extjs.txt or this above './'; 'extjs/' is for HTML
+       ,fading:!true             // visual effects for content appearance
     },
-    backend: {
+    backend:{
         file: 'app_main/app_back.js',
         ctl_port: 3008,
         job_port: 3007,
         sess_puzl: 'puzzle-word$50000X'
-    },
-    mongodb: {
+    }
+   ,mongodb:{
          host: 'localhost'
         ,port: 27017
         ,journal: true
