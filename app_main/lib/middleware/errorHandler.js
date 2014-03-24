88175a48860d29766507ccabdf38de01ae052757
Oleg Verych <olecom@gmail.com>
2014-03-24 13:44:02 +0200

    errorHandler: bug missing "+"; print all && stack if exists

--- app_main/lib/middleware/errorHandler.js
+++ app_main/lib/middleware/errorHandler.js
@@ -4,10 +4,10 @@ function errorHandler(err, req, res, next){
 
     if (err.status) res.statusCode = err.status
     if (res.statusCode < 400) res.statusCode = 500
-    api._err('ErrorHandler:')
-    err.url = req.url
-    err = api.ipt(err, { depth: 4 }) + '\nStack:\n'
-          api.ipt(err.stack)
+
+    err = 'ErrorHandler URL: ' + req.url + '\n' + err + (
+        err.stack ? '\nStack:\n' + err.stack : ''
+    )
     api._err(err)
     res.writeHead(res.statusCode, res.ContentTypes.TextPlain)
 
