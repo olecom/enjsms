6fb070e92fb76d0f4e0f884059bf476c997ccdcc
Oleg Verych <olecom@gmail.com>
2014-03-25 13:32:32 +0200

    controller.Main: event handlers refactoring
    remove useless refs, move handlers inside init() scope
    and use them (kind of private methods)

--- app_main/controller/Main.js
+++ app_main/controller/Main.js
@@ -19,6 +19,7 @@ Ext.define('App.controller.Main',{
     ],*/
     init: function(){
         var me = this
+
         if(me.inited){
             return
         }
@@ -26,47 +27,49 @@ Ext.define('App.controller.Main',{
 
         me.listen({
             global:{
-                updateVersions: me.updateVersions
-               ,userStatus: me.handleUserStatus
+                updateVersions: updateVersions
+               ,userStatus: handleUserStatus
             },
             controller:{ },
             component:{ },
             store:{ }
         })
+        return
 
-    }
-   ,handleUserStatus: function handleUserStatus(item){
-        App.sts(
-            'userstatus',
-            item.itemId,
-            l10n.stsOK,
-            new Date
-        )
-    }
-    ,updateVersions: function updateVersions(){
+        function handleUserStatus(item){
+            App.sts(
+                'userstatus',
+                item.itemId,
+                l10n.stsOK,
+                new Date
+            )
+        }
+
+        function updateVersions(){
         var el = Ext.get('versions'), _2 = { opacity: 0 }
 
-        el.animate({
-            to: _2,
-            duration: 1024,
-            callback: function versionFadeOut(){
-                el.setHTML(('-= versions =-\n'+
+            el.animate({
+                to: _2,
+                duration: 1024,
+                callback: function versionFadeOut(){
+                    el.setHTML(('-= versions =-\n'+
 'extjs:,' + Ext.versions.extjs.version + '\n' +
            (App.cfg.backend.url ?
 'nodejs:,' + App.cfg.backend.versions.node +
 'connectjs:,' + App.cfg.backend.versions.connectjs +
 'node-webkit:,'+ App.cfg.backend.versions.nw : '')
-            ).replace(/\n/g,'</b><br>').replace(/,/g, '<br><b>')
-                )
-                _2.opacity = 1
-                el.animate({
-                    to: _2,
-                    duration: 1024,
-                    callback: function versionFrameAnim(){
-                        el.frame("00FF00", 1, { duration: 1024 })
-                    }
-                })
-            }
-        })
-    }
+                ).replace(/\n/g,'</b><br>').replace(/,/g, '<br><b>')
+                    )
+                    _2.opacity = 1
+                    el.animate({
+                        to: _2,
+                        duration: 1024,
+                        callback: function versionFrameAnim(){
+                            el.frame("00FF00", 1, { duration: 1024 })
+                        }
+                    })
+                }
+            })
+        }
+    }// init()
 })
