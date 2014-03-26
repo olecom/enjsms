3417c5391f752c83340c96b89137b4c9353a421d
Oleg Verych <olecom@gmail.com>
2014-03-26 02:18:56 +0200

    App.backend.req (channel#1): fix `nw` case (local UI, remote backend)

--- app_main/backend/Connection.js
+++ app_main/backend/Connection.js
@@ -4,6 +4,7 @@
 Ext.define('App.backend.Connection',{
     extend: 'Ext.data.Connection',
     method: 'POST',
+    url: App.cfg.backend ? App.cfg.backend.url : '/',
     defaultHeaders:{
         'Content-Type': 'text/plain; charset=utf-8'
     }
@@ -15,6 +16,11 @@ Ext.define('App.backend.Connection',{
 App.backend.req = (function create_backend(conn){
     /* channel#1: request data from backend */
     return function backend_request(opts){
+        if(opts.url){
+            if(0 != opts.url.indexOf('http')){
+                opts.url = conn.url + opts.url
+            }
+        }
         return conn.request(opts)
     }
 })(Ext.create('App.backend.Connection'))
