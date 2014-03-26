b971ff44751ee00c63376f0cda645aa5da73f0c4
Oleg Verych <olecom@gmail.com>
2014-03-26 11:05:10 +0200

    ServerResponse: `res.js()` send components or arbitrary JS to UI front

--- app_main/lib/response.js
+++ app_main/lib/response.js
@@ -2,6 +2,7 @@ var http = require('http')
 
 http.ServerResponse.prototype.ContentTypes = {
     AppJSON:   { 'Content-Type': 'application/json; charset=utf-8' },
+    AppJS:     { 'Content-Type': 'application/javascript; charset=utf-8' },
     TextPlain: { 'Content-Type': 'text/plain; charset=utf-8' }
 }
 
@@ -19,4 +20,20 @@ function res_json(obj){
     this.setHeader('Content-Length', Buffer.byteLength(obj))
     this.writeHead(this.statusCode, this.ContentTypes.AppJSON)
     this.end(obj)
+}
+
+http.ServerResponse.prototype.js =
+/*  res.json({ success: true })
+ *  res.json('{ "success": true }')
+ *  res.json(401, { msg: ' Authorization Required' })
+ */
+function res_js(code){
+    if(!code){
+        code = ';/* No Code or Unauthorized */;'
+    } else if('string' != typeof obj){
+        code = code.toString()
+    }
+    this.setHeader('Content-Length', Buffer.byteLength(code))
+    this.writeHead(this.statusCode, this.ContentTypes.AppJS)
+    this.end(code)
 }
