95d04a43c298bb164ffe0905b4cd8d4db55141d6
Oleg Verych <olecom@gmail.com>
2014-04-02 01:17:44 +0200

    ctl.Userman: more view.Login UI action fixes
    addition to 287e5355f9f341c301944cfdfc68b29bf716b1bc
    refactor code on bottom

--- app_modules/userman/controller/Userman.js
+++ app_modules/userman/controller/Userman.js
@@ -25,23 +25,7 @@ Ext.define('App.controller.Userman', {
 
         User = App.model.userman.User// must be required first from app module
 
-        user.onTriggerClick = function onSessionShutdownClick(){
-            role.reset()
-            auth.disable()
-            auth.setText(l10n.um.loginOk)
-            user.focus()
-            user.setHideTrigger(true)
-            Ext.Msg.alert({
-                icon: Ext.Msg.INFO,
-                buttons: Ext.Msg.OK,
-                title: l10n.um.logoutTitle,
-                msg: l10n.um.logoutMsg(user.emptyText)
-            })
-            user.emptyText = l10n.um.loginUserBlank
-            user.reset()
-            User.logout()
-        }
-
+        user.onTriggerClick = onSessionShutdownClick
         user.focus()
         user.on({
             /* using `ref`s, but this is equivalent to:
@@ -73,35 +57,13 @@ Ext.define('App.controller.Userman', {
         me.listen({
             global:{
                 /* runApp: Login widget is `singleton: true` and runs already */
-                logout: function(){
-                    Ext.Msg.alert({
-                        icon: Ext.Msg.INFO,
-                        buttons: Ext.Msg.OK,
-                        title: l10n.um.logoutTitle,
-                        msg: l10n.um.logoutMsg(user.emptyText),
-                        fn: function(){
-                            User.logout(function(){
-                                location.reload(true)
-                            })
-                        }
-                    })
-                }
+                logout: logout
             }
             //,controller: { }
             //,store: {}
         })
 
-        User.login('?', function getSessionInfo(ret){
-            if(ret.can){
-                user.emptyText = ret.user.id
-                user.applyEmptyText()
-                user.setHideTrigger(false)
-                role.setValue(l10n.um.roles[ret.can.__name] || ret.can.__name)
-                auth.setText(l10n.um.loginCurrentSession)
-                auth.enable()
-                return// auth is ok in this session
-            }
-        })
+        User.login('?', getSessionInfo)// ask backend for current session
 
         me.listen({
             global:{
@@ -111,6 +73,20 @@ Ext.define('App.controller.Userman', {
         })
 
         return
+
+        function getSessionInfo(ret){
+            if(ret.can){
+                user.emptyText = ret.user.id
+                user.applyEmptyText()
+                user.setHideTrigger(false)
+                role.suspendEvents()// prevent e.g. pass.enable()
+                role.setValue(l10n.um.roles[ret.can.__name] || ret.can.__name)
+                role.resumeEvents()
+                auth.setText(l10n.um.loginCurrentSession)
+                auth.enable()
+                return// auth is ok in this session
+            }
+        }
 
         //
         function handleBackendEvents(success, res){
@@ -132,9 +108,30 @@ Ext.define('App.controller.Userman', {
         }
 
         // auth data actions
+        function onSessionShutdownClick(ev){
+            User.logout()
+            ev && Ext.Msg.alert({
+                icon: Ext.Msg.INFO,
+                buttons: Ext.Msg.OK,
+                title: l10n.um.logoutTitle,
+                msg: l10n.um.logoutMsg(user.emptyText)
+            })
+            user.emptyText = l10n.um.loginUserBlank
+            user.reset()
+            user.focus()
+            user.setHideTrigger(true)
+            role.store.removeAll(true)
+            role.reset()
+            auth.disable()
+            auth.setText(l10n.um.loginOk)
+        }
+
         function reqRole(field, newUserId){
             if(defer) clearTimeout(defer)
             if(newUserId){
+                if(!auth.disabled && newUserId != user.emptyText){
+                    onSessionShutdownClick()
+                }
                 defer = setTimeout(function deferReqRoles(){
                     defer = 0
                     User.login(newUserId, function getSessionInfo(ret){
@@ -143,11 +140,14 @@ Ext.define('App.controller.Userman', {
                             return// auth is ok in this session
                         }
 
-                        if(!ret.roles.length){
-                            if(!role.disabled){
+                        if(!ret.roles.length){// no user or roles
+                            if(!role.disabled){// if UI has something already
                                 role.store.removeAll(true)
                                 role.reset()
                                 role.disable()
+                                pass.reset()
+                                pass.disable()
+                                auth.disable()
                             }
                             return
                         }
@@ -167,10 +167,13 @@ Ext.define('App.controller.Userman', {
                 }, 512)
                 return
             }
-            if(!role.disabled){
+            if(!role.disabled){// empty user id
                 role.store.removeAll(true)
                 role.reset()
                 role.disable()
+                pass.reset()
+                pass.disable()
+                auth.disable()
             }
         }
         function authenticate(field, ev){
@@ -242,7 +245,19 @@ Ext.define('App.controller.Userman', {
         function enableAuth(){
             auth.enable()
         }
-
+        function logout(){
+            Ext.Msg.alert({
+                icon: Ext.Msg.INFO,
+                buttons: Ext.Msg.OK,
+                title: l10n.um.logoutTitle,
+                msg: l10n.um.logoutMsg(User.get('id')),
+                fn: function(){
+                    User.logout(function(){
+                        location.reload(true)
+                    })
+                }
+            })
+        }
     },
     destroy: function destroy(){
         this.callParent(arguments)
