067279c05ddbd1b21330b9c5e7a4c29e6bb97cb3
Oleg Verych <olecom@gmail.com>
2014-05-21 12:09:11 +0200

    app_module loader: more comments; less complex error reporting; coding style

--- app_modules/index.js
+++ app_modules/index.js
@@ -1,29 +1,27 @@
-function app_modules(api){
-    /* Application module loader */
-    var m = '', err = '', cfg = ''
-       ,fs = require('fs')
+module.exports = app_modules
+
+function app_modules(api){// Application modules loader
+var m = '', err = '', cfg = ''
+   ,fs = require('fs')
 
     for(m in api.cfg.app.modules){
-        try {// load: app_modules/userman/app_back_userman.js
+        try {// stat on FS e.g.: app_modules/userman/
             if(fs.statSync(__dirname + '/' + m).isDirectory()){
                 cfg = api.cfg.app.modules[m]
+                //e.g.: app_modules/userman/app_back_userman.js
                 m = './' + m + '/app_back_' + m + '.js'
             }
-        } catch(ex){
-            err = err || 'Error load app module(s) from `config`:\n'// ':' is used
-        }
-        if(!cfg){// load: app_modules/pingback.js
+        } catch(ex){ /* directory check failed */ }
+        if(!cfg){// will try to load file e.g.: app_modules/pingback.js
             cfg = api.cfg.app.modules[m]
             m = './' + m + '.js'
         }
-        try {
-            require(m)(api, cfg)
+        try {// to load the module
+            require(m)(api, cfg)// `cfg` isn't used so far in app modules...
         } catch(ex){
             err += m.replace(/[.]js/, '[.js]') + ':\n!!!' + ex.stack + '\n'
         }
         cfg = ''
     }
-    if(':' != err[err.length - 2]) api._err(err)
+    err && api._err('Error load app module(s) from `config`:\n', err)
 }
-
-module.exports = app_modules
