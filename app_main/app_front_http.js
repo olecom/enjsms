4e164c3264cdc7b2d22c425d3ed2175b5bf703f2
Oleg Verych <olecom@gmail.com>
2014-07-27 17:14:12 +0300

    app_front_http: rework to use no circular dep. via variable

--- app_main/app_front_http.js
+++ app_main/app_front_http.js
@@ -2,50 +2,49 @@
  *  'nodejs + connectjs': HTTP communication with backend (remote web browser)
  */
 
-(function get_extjs_remote_config(url){
+(function app_front_http(url){
 var xhr = new XMLHttpRequest
-    xhr.open(
-        'GET'
-        ,(url ? url : '') + '/app.config.extjs.json'
-        ,true
-    )
-    xhr.onreadystatechange = onreadystatechange
+    xhr.open('GET', (url ? url : '') + '/app.config.extjs.json', true)
+    xhr.onreadystatechange = load_config_then_ExtJS
     xhr.send()
+    xhr = null
     return
 
-    function onreadystatechange(res){
+    function load_config_then_ExtJS(res){
     var extjs_config, req = res.target
-        if(4 == req.readyState){
-            if(200 != req.status){
-                throw new Error(xhr ? l10n.errload_config_read : l10n.extjsNotFound)
-            } else {
-                if(xhr){
-                    xhr = null
-                    extjs_config = JSON.parse(req.responseText)
-                    if(url){// `nw` context
-                        app.config.extjs.load = extjs_config.load
-                        app.extjs_load(document, window)
-                        return
-                    }
-                    // `browser` context
-                    app.config = {
-                        extjs: extjs_config,
-                        backend:{// record start time
-                            time: new Date,
-                            msg: l10n.stsBackendXHR,
-                            op: l10n.stsCheck
-                        }
-                    }
-                    req.open(// check for network availability of ExtJS
-                        'HEAD'
-                       ,(url ? url : '') + app.config.extjs.path + 'ext-all-nw.js'
-                       ,true
-                    )
-                    req.send()
-                } else {
-                    app.extjs_load(document, window)
-                }
+        if(4 != req.readyState) return
+        if(200 != req.status) throw new Error(/config/.test(req.responseURL)?
+            l10n.errload_config_read :
+            l10n.extjsNotFound
+        )
+
+        if(req.responseText){// config
+            extjs_config = JSON.parse(req.responseText)
+            if(url){
+        // `nw` context
+                app.config.extjs.load = extjs_config.load
+                app.extjs_load(document, window)
+                return
+            }
+        // `browser` context
+        } else {// load after HEAD check
+            app.extjs_load(document, window)
+            return
+        }
+
+        app.config = {
+            extjs: extjs_config,
+            backend:{// record start time
+                time: new Date,
+                msg: l10n.stsBackendXHR,
+                op: l10n.stsCheck
             }
         }
+        req.open(// check for network availability of ExtJS
+            'HEAD'
+           ,(url ? url : '') + app.config.extjs.path + 'ext-all-nw.js'
+           ,true
+        )
+        req.send()
     }
 })(app.config && app.config.backend.url)
