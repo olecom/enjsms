1e25047a1c17153fe99476d3ef84947706479b04
Oleg Verych <olecom@gmail.com>
2014-08-21 16:04:47 +0300

    elaborate dee999e5b8cf94fe37781087d0f33242864f4498

--- app_main/lib/process.js
+++ app_main/lib/process.js
@@ -1,10 +1,12 @@
 function process_uglify_js(process, global){
 
-    global.__res = null// catch request to inform UI
+    global.__res = null// catch request to inform UI, see note in 'uncaughtException'
 
     process.on('uncaughtException', function(err){
         log('Caught exception: ' + err.stack)
         if(__res){
+        // NOTE: with many users and high load this may send errors to
+        //       the wrong end; thus this is mostly a development tool
             __res.json({success:false, data: err.stack})
             __res = null
         }
