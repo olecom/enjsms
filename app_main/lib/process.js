a64bf642fea8b8ab664d05c5dbc8eaafae3c2d8c
Oleg Verych <olecom@gmail.com>
2014-08-26 18:41:26 +0300

    catch errors, fix: dee999e5b8cf94fe37781087d0f33242864f4498

--- app_main/lib/process.js
+++ app_main/lib/process.js
@@ -3,13 +3,15 @@ function process_uglify_js(process, global){
     global.__res = null// catch request to inform UI, see note in 'uncaughtException'
 
     process.on('uncaughtException', function(err){
+    try{
         log('Caught exception: ' + err.stack)
         if(__res){
-        // NOTE: with many users and high load this may send errors to
-        //       the wrong end; thus this is mostly a development tool
+    // NOTE: with many users and high load this may send errors to
+    //       the wrong end; thus this is mostly a development tool
             __res.json({success:false, data: err.stack})
             __res = null
         }
+    }catch(ex){ }
     })
 
     process.on('exit', function process_exit(){
